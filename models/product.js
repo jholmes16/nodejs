d45dc31ffaf1e2536aa177f0e3ea15f041bc6de2
Jahmaul Holmes <jholmes16@users.noreply.github.com>
2021-02-10 15:59:31 -0500

    Add files via upload

--- models/product.js
+++ models/product.js
@@ -1,32 +1,66 @@
-const db = require('../util/database');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const Cart = require('./cart');
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
+class Product {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
     this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectID(id) : null;//covert id to a string
+    this.userId = userId;
   }
-  //? marks security for sql injection
+
   save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-    [this.title, this.price, this.imageUrl, this.description]
-    );    
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      // Update the product; "this" keyword will replace all the fields of the object
+      dbOp = db
+      .collection('products')
+      .updateOne( {_id: this._id }, {$set: this});
+    } else {
+      dbOp = db
+      return db.collection('products').insertOne(this);
+    }
+    return dbOp
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
   }
 
-  static deleteById(id) {
-    
-  }
-
+  //finds all you products
   static fetchAll() {
-   return db.execute('SELECT * FROM products');
+    const db = getDb();
+    return db.collection('products').find().toArray()
+    .then(products => {
+      console.log(products);
+      return products;
+    })
+    .catch(err => console.log(err));
   }
 
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+    .find({ _id: new mongodb.ObjectId(prodId) })
+    .next()
+    .then(product => {
+      console.log(product);
+      return product;
+    })
+    .catch(err => console.log(err));
   }
-};
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return  db.collection('products').deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then(result => {
+      console.log('Deleted');
+    })
+    .catch(err => console.log(err));
+  }
+}
+
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,10 +1,26 @@
-const mysql = require('mysql2');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'password'
-});
+let _db;
 
-module.exports = pool.promise();
+const mongoConnect = (callback) => {MongoClient.connect('mongodb+srv://Chicago01:Chicago2@cluster0.5acga.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected!');
+    _db = client.db();//you can pass a parameter in client.db to connect to different db
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw (err);
+  });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
